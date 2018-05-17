94a89394603e4a46066c59e68421484fc86c1096
Your Name <you@example.com>
2018-05-17 05:35:10 -0400

    import user.model et user.service + ajout des fonctions getUser() et save(user)

--- client/app/film-detail/film-detail.component.ts
+++ client/app/film-detail/film-detail.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Film } from '../shared/models/film.model';
+import { User } from '../shared/models/user.model';
 import { ActivatedRoute } from '@angular/router';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
-
+import { AuthService } from '../services/auth.service';
 import { FilmService } from '../services/film.service';
 
+import { UserService } from '../services/user.service';
+import { ToastComponent } from '../shared/toast/toast.component';
 @Component({
   selector: 'app-film-detail',
   templateUrl: './film-detail.component.html',
   styleUrls: ['./film-detail.component.css']
 })
 export class FilmDetailComponent implements OnInit {
-  @Input() film: Film;
+  @Input() user: User;
+  film = new Film();
+  films: Film[] = [];
+  isLoading = true;
+  
   constructor(
   private route: ActivatedRoute,
-
+  public toast: ToastComponent,
   private filmService: FilmService,
+  private  userService: UserService,
+  private auth: AuthService,
   private location: Location
 ) {}
 
   ngOnInit(): void {
   this.getFilm();
+  this.getUser();
 }
 // paramMap.get('id');
 // params.urltorrent;
@@ -30,6 +40,23 @@ export class FilmDetailComponent implements OnInit {
  this.filmService.getFilm(ligne)
     .subscribe(film => this.film = film);
 
+}
+
+
+ getUser() {
+    this.userService.getUser(this.auth.currentUser).subscribe(
+      data => this.user = data,
+      error => console.log(error),
+      () => this.isLoading = false
+    );
+}
+
+
+save(user: User) {
+    this.userService.editUser(user).subscribe(
+      res => this.toast.setMessage('account settings saved!', 'success'),
+      error => console.log(error)
+    );
 }
 
 
