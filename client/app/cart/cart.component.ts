4fa0babf7e56539751ba6ab7acab084876f68b84
Your Name <you@example.com>
2018-05-29 14:21:58 -0400

    ...

---
+++ client/app/cart/cart.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input  } from '@angular/core';
+import { ToastComponent } from '../shared/toast/toast.component';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { User } from '../shared/models/user.model';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+
+
+export class CartComponent implements OnInit {
+
+  user: User;
+  isLoading = true;
+
+  constructor(private auth: AuthService,
+              public toast: ToastComponent,
+              private userService: UserService) { }
+
+  ngOnInit() {
+    this.getUser();
+  }
+
+  getUser() {
+    this.userService.getUser(this.auth.currentUser).subscribe(
+      data => this.user = data,
+      error => console.log(error),
+      () => this.isLoading = false
+    );
+  }
+
+}
