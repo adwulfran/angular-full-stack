0392a26402b6a945745b837c17566413f5312391
Your Name <you@example.com>
2018-05-29 14:13:47 -0400

    definition de items comme Array

--- server/models/user.ts
+++ server/models/user.ts
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true, trim: true },
   password: String,
   role: String,
-  items : String
+  items : Array
 });
 
 // Before saving the user, hash the password
