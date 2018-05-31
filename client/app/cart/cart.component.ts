5439fcb571aa8b947587072a6c3027fd9f44edfa
Your Name <you@example.com>
2018-05-31 03:55:24 -0400

    ajout fonction deletecart(user)

--- client/app/cart/cart.component.ts
+++ client/app/cart/cart.component.ts
@@ -3,6 +3,8 @@ import { ToastComponent } from '../shared/toast/toast.component';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
 import { User } from '../shared/models/user.model';
+import { Film } from '../shared/models/film.model';
+import { FilmService } from '../services/film.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -16,6 +18,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class CartComponent implements OnInit {
 
   user: User;
+  film = new Film();
+  films: Film[] = [];
   isLoading = true;
 
   constructor(private auth: AuthService,
@@ -33,5 +37,12 @@ export class CartComponent implements OnInit {
       () => this.isLoading = false
     );
   }
+
+  deletecart(user: User) {
+    this.userService.deletecart(user).subscribe(
+      res => this.toast.setMessage('account settings saved!', 'success'),
+      error => console.log(error)
+    );
+}
 
 }
