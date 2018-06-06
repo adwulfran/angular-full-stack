d85920d9a0f20c8e26a91578ac2bf132aaa58c4d
Your Name <you@example.com>
2018-06-06 09:55:22 -0400

    ajout userService et userService.getUser

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,9 @@
 import { AfterViewChecked, ChangeDetectorRef, Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+//ajout de UserService...
+import { UserService } from './services/user.service';
+import { User } from './shared/models/user.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,7 +11,10 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements AfterViewChecked {
 
+ 	user: User;
+ 	 isLoading = true;	
   constructor(public auth: AuthService,
+  			  public userService : UserService,
               private changeDetector: ChangeDetectorRef) { }
 
   // This fixes: https://github.com/DavideViolante/Angular-Full-Stack/issues/105
@@ -15,4 +22,16 @@ export class AppComponent implements AfterViewChecked {
     this.changeDetector.detectChanges();
   }
 
+// ajout ...
+   ngOnInit() {
+    this.getUser();
+}
+  	// ajout de getUser...
+	 getUser() {
+    this.userService.getUser(this.auth.currentUser).subscribe(
+      data => this.user = data,
+      error => console.log(error),
+      () => this.isLoading = false
+    );
+	}
 }
