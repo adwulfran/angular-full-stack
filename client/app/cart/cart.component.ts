0fad990b18783be87620d3148cb52f5818a10177
adwulfran <adrienwulfran@gmail.com>
2018-07-10 06:40:05 -0400

    + DatacartService + creation getUseritems() + ajout dans getUser()

--- client/app/cart/cart.component.ts
+++ client/app/cart/cart.component.ts
@@ -4,9 +4,12 @@ import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
 import { User } from '../shared/models/user.model';
 import { Film } from '../shared/models/film.model';
+import { Useritems } from '../shared/models/useritems.model';
 import { FilmService } from '../services/film.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DatacartService } from "../services/datacart.service";
+
 
 @Component({
   selector: 'app-cart',
@@ -17,17 +20,33 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 export class CartComponent implements OnInit {
 
-  user: User;
+ user = new User();
   film = new Film();
   films: Film[] = [];
   isLoading = true;
+  message: any;
+  timeout: number = 1;
+  useritems : Useritems[] = [];
+  pets: any[] = [];
+
 
   constructor(private auth: AuthService,
               public toast: ToastComponent,
+              private data: DatacartService,
               private userService: UserService) { }
+
 
   ngOnInit() {
     this.getUser();
+    this.data.currentMessage.subscribe(message => this.message = message); 
+    
+    
+  }
+
+  newMessage() {
+    
+      this.data.changeMessage(0);
+       
   }
 
   getUser() {
@@ -36,14 +55,37 @@ export class CartComponent implements OnInit {
       error => console.log(error),
       () => this.isLoading = false
     );
+    setTimeout(() => {
+      this.getUseritems();
+}, 1000); 
+    
+    
+}
+
+
+  getUseritems() { 
+   let pets: any[] = this.user.items;
+  console.log("ICI ON ATTEND "+JSON.stringify(this.pets)); 
+    for (let pet of pets) {
+        this.useritems.push(pet); // "0", "1", "2",
+    }
+    console.log("ICI ON ATTEND??? "+JSON.stringify(this.useritems)); 
+
+    
   }
 
+  
   deletecart(user: User) {
+   
     this.userService.deletecart(user).subscribe(
       res => this.toast.setMessage('account settings saved!', 'success'),
       error => console.log(error)
     );
+   
 }
+
+
+
 
 
 
