196cddb7e7e22739e8c1ab8aa6e61520fda9ea6a
adwulfran <adrienwulfran@gmail.com>
2018-07-10 06:36:21 -0400

    + DatacartService

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,9 +1,6 @@
 import { AfterViewChecked, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
-//ajout de UserService...
-import { UserService } from './services/user.service';
-import { User } from './shared/models/user.model';
-
+//+ DatacartService...
 import { DatacartService } from "./services/datacart.service";
 @Component({
   selector: 'app-root',
@@ -11,12 +8,11 @@ import { DatacartService } from "./services/datacart.service";
 })
 export class AppComponent implements AfterViewChecked {
 
-   user: User;
-   message:number;
+   // + message containing user.items.length; 
+   message:any;
    isLoading = true;  
   constructor(public auth: AuthService,
-          public userService : UserService,
-          private data: DatacartService,
+              private data: DatacartService,
               private changeDetector: ChangeDetectorRef) { }
 
   // This fixes: https://github.com/DavideViolante/Angular-Full-Stack/issues/105
@@ -26,15 +22,9 @@ export class AppComponent implements AfterViewChecked {
 
 // ajout ...
    ngOnInit() {
-    this.getUser();
+    //this.getUser();
     this.data.currentMessage.subscribe(message => this.message = message);
-}
-    // ajout de getUser...
-   getUser() {
-    this.userService.getUser(this.auth.currentUser).subscribe(
-      data => this.user = data,
-      error => console.log(error),
-      () => this.isLoading = false
-    );
   }
+    
+
 }
